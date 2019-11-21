c8b012c7c892a916be269f60cb1aefd0d167c349
andys <andysmith836@gmail.com>
2019-11-21 22:35:10 +0000

    Clean up

--- src/stringcalc_clj/calc.clj
+++ src/stringcalc_clj/calc.clj
@@ -6,7 +6,7 @@
 
 (defn convert-to-valid-ints [values matcher]
   (->> (str/split values matcher)
-       (filter #(not (= "" %)))
+       (filter #(not (str/blank? %)))
        (map #(Integer/parseInt %))
        (filter #(< % 1001)))
   )
@@ -25,22 +25,19 @@
     )
   )
 
-(defn sum-values-custom-delimiter [values delimiter]
-  (let [matcher (re-pattern (str delimiter "|\n"))]
-    (sum-values values matcher)
-    )
-  )
+(defn sum-values-custom-separators [values separators]
+  (sum-values values (re-pattern (str separators "|\n"))))
 
-(defn has-delimiter? [input]
-  (and (str/includes? input "//") (str/includes? input "\n")))
-
-(defn get-delimiter [input]
+(defn get-separator-string [input]
   (let [delimiter (subs input (+ (str/index-of input "//") 2) (str/index-of input "\n"))]
     (str/replace delimiter "][" "]|[")
-  ))
+    ))
+
+(defn has-separator? [input]
+  (and (str/includes? input "//") (str/includes? input "\n")))
 
 (defn get-rest-of-string [values]
-  (if (has-delimiter? values)
+  (if (has-separator? values)
     (let [pos-of-new-line (str/index-of values "\n")]
       (if (= pos-of-new-line nil)
         values
@@ -52,8 +49,8 @@
   )
 
 (defn get-control-string [input]
-  (if (has-delimiter? input)
-    (get-delimiter input)
+  (if (has-separator? input)
+    (get-separator-string input)
     ","
     )
   )
@@ -61,7 +58,7 @@
 (defn total-values [values]
   (let [delimiter (get-control-string values)
         rest-of-string (get-rest-of-string values)]
-    (sum-values-custom-delimiter rest-of-string delimiter)
+    (sum-values-custom-separators rest-of-string delimiter)
     )
   )
 
